b8aa6bdfd469e55db3c73f3c144617c077966112
Ray Elder <hello@rayelder.com>
2023-02-28 12:59:03 -0800

    Update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ weight: 'variable',subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ray Elder: Creative Technologist</title>
-        <meta name="description" content="Ray Elder: Creative Technologist" />
+        <title>Ray Elder</title>
+        <meta name="description" content="Ray Elder" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main><h1 className={inter.className}>Ray Elder</h1></main>
+        <h1 className={inter.className} style={{margin: "0", padding: "0 0 0 .125rem", fontSize:"1.125rem", fontWeight: "500"}}>Ray Elder <span style={{paddingLeft: "0.5rem", fontWeight: "200"}}>Creative Technologist</span></h1>
     </>
   )
 }
