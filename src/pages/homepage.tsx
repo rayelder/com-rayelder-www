a8646f7ece8e98c3354d25d1961e078e8095a047
Ray Elder <hello@rayelder.com>
2023-02-28 22:08:08 -0800

    Create temporary landing page

---
+++ src/pages/homepage.tsx
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { Inter } from 'next/font/google'
+// import { getEssays } from 'sanity'
+import { client } from 'sanity'
+
+const inter = Inter({ weight: 'variable',subsets: ['latin'] })
+
+type Essay = {
+  slug: any,
+  headline: string,
+}
+
+type Essays = {
+  essays: Essay[];
+}
+
+export default function Homepage({essays}: Essays) {
+
+  // console.log(essays[0].slug);
+  
+  
+  return (
+    <>
+      <Head>
+        <title>Ray Elder</title>
+        <meta name="description" content="Ray Elder" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <h2 className={inter.className}>
+        <Link href="/">Ray Elder</Link>
+      </h2>
+      <ul className={inter.className} >
+          {
+          essays.map((essay,index) => (
+            <li key={index}>
+              <Link href={`essays/${essay.slug.current}`}>{essay.headline}</Link>
+            </li>
+          ))
+          }
+        </ul>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const essays = await client.fetch('*[_type == "essay"]')
+  return {
+    props: {
+      essays,
+    },
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,6 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import { Inter } from 'next/font/google'
-// import { getEssays } from 'sanity'
-import { client } from 'sanity'
 
-const inter = Inter({ weight: 'variable',subsets: ['latin'] })
-
-type Essay = {
-  slug: any,
-  headline: string,
-}
-
-type Essays = {
-  essays: Essay[];
-}
-
-export default function Home({essays}: Essays) {
-
-  // console.log(essays[0].slug);
-  
-  
+export default function Index() {
   return (
     <>
       <Head>
@@ -27,27 +8,16 @@ export default function Home({essays}: Essays) {
         <meta name="description" content="Ray Elder" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <h2 className={inter.className}>
-        <Link href="/">Ray Elder</Link>
-      </h2>
-      <ul className={inter.className} >
-          {
-          essays.map((essay,index) => (
-            <li key={index}>
-              <Link href={`essays/${essay.slug.current}`}>{essay.headline}</Link>
-            </li>
-          ))
-          }
-        </ul>
+      <div id="temporary">
+      <h1>I’m working on something different</h1>
+      <div className="rules">
+        <div className="rule"></div>
+        <div className="rule"></div>
+        <div className="rule"></div>
+        <div className="rule"></div>
+      </div>
+      <p>Ray Elder is a creative technologist living in Southern California.</p>
+    </div>
     </>
   )
-}
-
-export async function getStaticProps() {
-  const essays = await client.fetch('*[_type == "essay"]')
-  return {
-    props: {
-      essays,
-    },
-  }
 }
